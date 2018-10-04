f9dea76b14ce082a70916bc96a4a179331160cc3
Jayshol <jayshol@gmail.com>
2018-10-03 18:37:31 -0700

    Fixed error

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://fathomless-plains-59362.herokuapp.com/';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://fathomless-plains-59362.herokuapp.com';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://jayshol:shiridibaba123@ds155862.mlab.com:55862/puzzle-database';
 //exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/jwt-auth-demo';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://jayshol:shiridibaba123@ds157522.mlab.com:57522/test-puzzle-database';
